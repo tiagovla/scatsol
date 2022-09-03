aaaf85ffc2d1359a15fe5f0f0f42b771943f65a4
tiagovla <tiagovla@gmail.com>
2022-09-03 01:52:28 -0300

    ci: update version

--- docs/conf.py
+++ docs/conf.py
@@ -24,8 +24,8 @@ author = "tiagovla"
 
 # The full version, including alpha/beta/rc tags
 
-version = "0.0.1"
-release = "0.0.1"
+version = "0.0.2"
+release = "0.0.2"
 
 
 # -- General configuration ---------------------------------------------------
