d45704fe71d3ad3ee3febc06075d469c35d8286d
tiagovla <tiagovla@gmail.com>
2024-02-24 18:46:10 -0500

    chore: fix plotting typehints

--- scatsol/plotting.py
+++ scatsol/plotting.py
@@ -1,9 +1,11 @@
+from matplotlib import pyplot as plt
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
 from scatsol.geometry import CylindricalGeometry
-import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_geometry(geometry: CylindricalGeometry, show: bool = False):
+def plot_geometry(geometry: CylindricalGeometry, show: bool = False) -> tuple[Figure, Axes]:
     fig, ax = plt.subplots(constrained_layout=True)
     phi = np.linspace(0, 2 * np.pi, 100)
     for region in geometry.regions:
