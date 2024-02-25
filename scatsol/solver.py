chore: correct dielectric TM calculation

scatsol/solver.py
@@ -11,11 +11,15 @@ class CylindricalSolution:
         self,
         n: int,
         an: npt.NDArray[np.complex128],
+        cn: npt.NDArray[np.complex128],
+        dn: npt.NDArray[np.complex128],
         geometry: CylindricalGeometry,
         frequency: Annotated[float, "Hz"],
     ):
         self.n = n
         self.an = an
+        self.cn = cn
+        self.dn = dn
         self.geometry = geometry
         self.frequency = frequency
 
@@ -25,8 +29,30 @@ class CylindricalSolution:
         e_field = np.zeros_like(xyz, dtype=np.complex128)
         h_field = np.zeros_like(xyz, dtype=np.complex128)
 
-        raise NotImplementedError
+        nn = np.arange(0, self.an.shape[0])
+        rho = np.sqrt(xyz[:, 0] ** 2 + xyz[:, 1] ** 2)
+        phi = np.arctan2(xyz[:, 1], xyz[:, 0])
+        eps = 1.0 + np.heaviside(nn - 0.5, 1)
 
+        costerm = np.cos(nn[:, None] * phi)
+        sinterm = np.sin(nn[:, None] * phi)
+
+        for idx, region in enumerate(self.geometry.regions):
+            k = region.material.k(self.frequency)
+            mask = np.vectorize(region.check_within)(rho)
+            if region == self.geometry.regions[-1]:
+                besselterm = jv(nn[:, None], k * rho[mask])
+                e_field[mask, 2] = ((self.an + 1j**-nn) * eps) @ (besselterm * costerm[:, mask])
+            elif region == self.geometry.regions[0]:
+                besselterm = jv(nn[:, None], k * rho[mask])
+                e_field[mask, 2] = ((self.cn[:, 0] + self.dn[:, 0]) * eps) @ (
+                    besselterm * costerm[:, mask]
+                )
+            else:
+                besselterm_0 = hankel1(nn[:, None], k * rho[mask])
+                besselterm_1 = hankel2(nn[:, None], k * rho[mask])
+                e_field[mask, 2] = (self.cn[:, idx] * eps) @ (besselterm_0 * costerm[:, mask])
+                e_field[mask, 2] += (self.dn[:, idx] * eps) @ (besselterm_1 * costerm[:, mask])
         return e_field, h_field
 
     def incident_field(
@@ -75,6 +101,8 @@ class CylindricalSolver:
         n_regions = len(self.geometry.regions)
         a = np.array([region.outer_radius for region in self.geometry.regions[:-1]])
         dcn = np.zeros((n, n_regions))
+        cn = np.zeros((n, n_regions))
+        dn = np.zeros((n, n_regions))
         re = np.zeros((n, n_regions))
         an = np.zeros(n)
         nn = np.arange(0, n)
@@ -91,20 +119,45 @@ class CylindricalSolver:
             kiai = ki * a[i]
             kip1ai = kip1 * a[i]
 
+            amp = np.sqrt((mip1.epsilon * mi.mu) / (mip1.mu * mi.epsilon))
             num = hankel1(nn, kiai) + dcn[:, i] * hankel2(nn, kiai)
             den = h1vp(nn, kiai) + dcn[:, i] * h2vp(nn, kiai)
 
-            re[:, i] = np.sqrt((mip1.epsilon * mi.mu) / (mip1.mu * mi.epsilon)) * num / den
+            re[:, i] = amp * num / den
 
-            num = hankel1(nn, kip1ai) - re[:, i] * h1vp(nn, kip1ai)
-            den = hankel2(nn, kip1ai) - re[:, i] * h2vp(nn, kip1ai)
+            if i < n_regions - 1:
+                num = hankel1(nn, kip1ai) - re[:, i] * h1vp(nn, kip1ai)
+                den = hankel2(nn, kip1ai) - re[:, i] * h2vp(nn, kip1ai)
+                dcn[:, i + 1] = -num / den
 
-            dcn[:, i + 1] = -num / den
-
-        m = self.geometry.regions[-1].material
-        k = m.k(self.frequency)
-        kam = k * a[-1]
-        num = jv(nn, kam) - re[:, -1] * jvp(nn, kam)
-        den = hankel2(nn, kam) - re[:, -1] * h2vp(nn, kam)
+        kam = self.geometry.regions[-1].material.k(self.frequency) * a[-1]
+        kmam = self.geometry.regions[-2].material.k(self.frequency) * a[-1]
+        num = jv(nn, kam) - re[:, -2] * jvp(nn, kam)
+        den = hankel2(nn, kam) - re[:, -2] * h2vp(nn, kam)
         an = -(1j ** (-nn)) * num / den
-        return CylindricalSolution(n=n, an=an, geometry=self.geometry, frequency=self.frequency)
+
+        # founding cn and dn by going backwards
+        num = 1j ** (-nn) * jv(nn, kam) + an * hankel2(nn, kam)
+        den = hankel1(nn, kmam) + dcn[:, -2] * hankel2(nn, kmam)
+        cn[:, -2] = num / den
+        dn[:, -2] = dcn[:, -2] * cn[:, -2]
+
+        for i in range(n_regions - 3, -1, -1):
+            mi = self.geometry.regions[i].material
+            mip1 = self.geometry.regions[i + 1].material
+            ki = mi.k(self.frequency)
+            kip1 = mip1.k(self.frequency)
+            kiai = ki * a[i]
+            kip1ai = kip1 * a[i]
+
+            num = cn[:, i + 1] * hankel1(nn, kip1ai) + dn[:, i + 1] * hankel2(nn, kip1ai)
+            den = hankel1(nn, kiai) + dcn[:, i] * hankel2(nn, kiai)
+            cn[:, i] = num / den
+            dn[:, i] = dcn[:, i] * cn[:, i]
+
+        cn = np.nan_to_num(cn) # HACK: nan values should be correctly handled
+        dn = np.nan_to_num(dn)
+
+        return CylindricalSolution(
+            n=n, an=an, cn=cn, dn=dn, geometry=self.geometry, frequency=self.frequency
+        )

tests/test_scatsol.py
@@ -1,14 +1,17 @@
 from scatsol import __version__
-from scatsol.constant import Polarization
-from scatsol.geometry import CylindricalGeometry
-from scatsol.material import Material
-from scatsol.plotting import plot_geometry
-from scatsol.region import CylindricalRegion
-from scatsol.solver import CylindricalSolver
+from scatsol import (
+    Polarization,
+    CylindricalGeometry,
+    Material,
+    CylindricalRegion,
+    CylindricalSolver,
+)
 import numpy as np
 import scatsol
 
 import matplotlib.pyplot as plt
+
+from scatsol.plotting import plot_geometry
 
 
 def test_version():
@@ -16,28 +19,29 @@ def test_version():
 
 
 def test_problem():
-    free_space_m = Material(epsilon_r=1.0, mu_r=1.0)
-    cylinder_m = Material(epsilon_r=4.0, mu_r=1.0)
-    coating_m = Material(epsilon_r=3.0, mu_r=1.0)
+    diel = Material(epsilon_r=4.00, mu_r=1.0)
+    free_space = Material.free_space()
 
-    cylinder_r = CylindricalRegion(inner_radius=0.0, outer_radius=0.5, material=cylinder_m)
-    coating_r = CylindricalRegion(inner_radius=0.5, outer_radius=1.0, material=coating_m)
-    free_space_r = CylindricalRegion(
-        inner_radius=1.0, outer_radius=scatsol.INFTY, material=free_space_m
-    )
+    cylinder_r = CylindricalRegion(inner_radius=0.0, outer_radius=0.5, material=diel)
+    coating_r = CylindricalRegion(inner_radius=0.5, outer_radius=1.0, material=diel)
+    free_space_r = CylindricalRegion(1.0, float("inf"), material=free_space)
 
     geometry = CylindricalGeometry([cylinder_r, coating_r, free_space_r])
-    # plot_geometry(geometry=geometry, show=False)
+    # plot_geometry(geometry=geometry)
 
     solver = CylindricalSolver(geometry=geometry, frequency=300e6)
     solution = solver.solve(polarization=Polarization.TM, n=50)
-    x, y = np.meshgrid(np.linspace(-2, 2, 30), np.linspace(-2, 2, 30))
-    xyz = np.stack([x.ravel(), y.ravel(), np.zeros(x.size)], axis=1)
-    e_inc_field, _ = solution.incident_field(xyz)
 
-    fig, ax = plt.subplots()
-    ax.tripcolor(*xyz[:, np.array([0, 1])].T, e_inc_field[:, 2].real)
-    print(e_inc_field[:, 2].real)
+    x, y = np.meshgrid(np.linspace(-3, 3, 200), np.linspace(-3, 3, 200))
+    xyz = np.stack([x.ravel(), y.ravel(), np.zeros(x.size)], axis=1)
+    e_field, _ = solution.total_field(xyz)
+
+    _, ax = plt.subplots(constrained_layout=True)
+    ax.tripcolor(*xyz[:, [0, 1]].T, e_field[:, 2].real, shading="gouraud", vmin=-1, vmax=1)
+    for r in geometry.regions[:-1]:
+        phi = np.linspace(0, 2 * np.pi, 100)
+        ax.plot(r.outer_radius * np.cos(phi), r.outer_radius * np.sin(phi), "k")
+    ax.margins(0)
     plt.show()
 
     assert False